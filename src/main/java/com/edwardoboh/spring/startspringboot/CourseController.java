d886e53b130bcc84dbf336b610ae895fe234699c
edwardoboh <obohedward@gmail.com>
2023-07-27 20:37:06 +0100

    feat: finish up demo project

--- src/main/java/com/edwardoboh/spring/startspringboot/CourseController.java
+++ src/main/java/com/edwardoboh/spring/startspringboot/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StartSpringController {
+public class CourseController {
 	
 	@RequestMapping("/courses")
 	public List<Course> fetchAllCources(){
